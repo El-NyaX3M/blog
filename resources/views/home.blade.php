f0e4a3307a7d5487c739cc371bb19707b1d1b461
El-NyaX3M <niggel.x3m@gmail.com>
2024-11-07 16:06:22 -0700

    6- Plantillas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
+@extends('layouts.app')
+
+@section('title', 'tonotos')
+
+
+@section('content')
     <div class="container mx-auto px-4">
         <h1 class="color-blue">Página principal</h1>
-    
+
         
         <x-alert type="danger">
             <x-slot name="title">
@@ -19,6 +15,6 @@
             Me la pelan 
         </x-alert>
     </div>
+@endsection
     
-</body>
-</html>
+
